5eb90eef06d67d9f5493d4152e7cd0159c1c133a
inceptionJh <kjaehun6546@gmail.com>
2018-12-15 14:48:32 +0900

    Update todo database code

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ var signupRouter = require('./routes/signup');
 var signinRouter = require('./routes/signin');
 var signoutRouter = require('./routes/signout');
 var usersRouter = require('./routes/users');
+var todosRouter = require('./routes/todos');
 
 var app = express();
 
 // DB initialization.
-const init = require('./db');
+require('./db');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -32,31 +33,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Add headers
-// app.use(function(req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'X-Requested-With,content-type'
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
-
 app.use(
   session({
     secret: 'codestates present',
@@ -71,24 +47,20 @@ app.use('/signup', signupRouter);
 app.use('/signin', signinRouter);
 app.use('/signout', signoutRouter);
 app.use('/users', usersRouter);
+app.use('/todo', todosRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  // res.render('error');
   res.send('error');
 });
 
---
+++ models/todos/query.js
@@ -0,0 +1,22 @@
+const TABLE_NAME = "todos";
+
+module.exports = {
+  createTodo: ({ id, state, text }) => {
+    return `
+      INSERT INTO ${TABLE_NAME}(id, state, text)
+      VALUES(${id}, "${state}", "${text}")
+    `;
+  },
+
+  readTodo: id => `SELECT * FROM ${TABLE_NAME} WHERE id=${id}`,
+
+  readTodos: () => `SELECT * FROM ${TABLE_NAME}`,
+
+  updateTodo: ({ id, state, text }) => `
+    UPDATE ${TABLE_NAME}
+    SET state=${state}, text="${text}"
+    WHERE id=${id};
+  `,
+
+  deleteTodo: id => `DELETE FROM ${TABLE_NAME} WHERE id=${id}`
+};
